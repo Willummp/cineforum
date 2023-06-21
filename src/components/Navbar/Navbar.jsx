3058005b130d3bccda1ef1cfe23db7221360aee2
Willummp <112709123+Willummp@users.noreply.github.com>
2023-06-21 19:25:33 +0000

    commit 00120

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,10 +2,10 @@ import './Navbar.css'
 import React, { useState } from 'react';
 
 export default function Navbar() {
-  const [isVisible, setIsVisible] = useState(false);
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
+	const [isVisible, setIsVisible] = useState(false);
+	const toggleVisibility = () => {
+		setIsVisible(!isVisible);
+	};
 	return (
 		<>
 			<header className={`navbar ${isVisible ? 'navbar_expand' : ''}`}>
@@ -13,7 +13,7 @@ export default function Navbar() {
 				<p>Seu forum de cinema</p>
 				<div className={`navbar_items ${isVisible ? 'navbar_items_expand' : ''}`}>
 					<p className='navbar_items_expand_btn'> TRENDING TOPICS</p>
-					<p className='navbar_items_expand_btn'> RECENTES</p>
+					<p className='navbar_items_expand_btn'>  RECENTES</p>
 					<p className='navbar_items_expand_btn'> USUARIOS ONLINE</p>
 					<p className='navbar_items_expand_btn'> GITHUB</p>
 				</div>
